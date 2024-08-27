64a9f31a3432d869719c9dc57e2262af00bf92dd
Rick Pham <pmbaophuc@gmail.comgit config --global user.nam Rickgit config --global user.email pmbaophuc@gmail.com>
2024-08-27 00:11:04 -0600

    Add Array Challenge Activity 7.17.1.java

---
+++ Java/ArrayChallengeActivity7171.java
@@ -0,0 +1,19 @@
+//Declare an array list vowels that contains the five strings "a", "e", "i", "o", and "u".
+
+import java.util.ArrayList;
+
+public class ArrayChallengeActivity7171
+{
+   public static void main(String[] args)
+   {
+      ArrayList<String> vowels = new ArrayList<String>();
+      vowels.add("a");
+      vowels.add("e");
+      vowels.add("i");
+      vowels.add("o");
+      vowels.add("u");
+      /* Your code goes here */
+      
+      System.out.println("vowels: " + vowels);
+   }
+}
